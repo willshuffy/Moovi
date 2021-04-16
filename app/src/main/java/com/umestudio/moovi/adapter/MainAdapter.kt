ee3293510976d56e9d493bfd4705c117b59dbf07
willshuffy <saputrawilly26@gmail.com>
2021-04-16 16:11:16 +0700

    list movie adapter

---
+++ app/src/main/java/com/umestudio/moovi/adapter/MainAdapter.kt
@@ -0,0 +1,45 @@
+package com.umestudio.moovi.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.umestudio.moovi.R
+import com.umestudio.moovi.model.MovieModel
+
+/*
+    STEP 1 : - buat properties pada constructor
+             - alt+enter pada viewHolder
+             - buat class viewHolder
+ */
+class MainAdapter(var movies: ArrayList<MovieModel>): RecyclerView.Adapter<MainAdapter.viewHolder>() {
+
+    /*
+        STEP 3 : - alt+enter pada class MainAdapter
+                 - implement member onCreateViewHolder, getItemCount, onBindViewHolder
+                 - lengkapi method override
+     */
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int)= viewHolder (
+        LayoutInflater.from(parent.context).inflate(R.layout.item_movie, parent, false)
+    )
+
+
+    override fun getItemCount() = movies.size
+
+    override fun onBindViewHolder(holder: viewHolder, position: Int) {
+        holder.bind(movies[position])
+    }
+
+    /*
+        STEP 2 : - lengkapi constructor pada class viewHolder
+                 - tambahkan function
+     */
+
+    class viewHolder(view: View): RecyclerView.ViewHolder(view) {
+        fun bind(movies: MovieModel){
+
+        }
+
+    }
+}

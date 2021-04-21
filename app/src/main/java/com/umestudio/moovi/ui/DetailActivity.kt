9b35f758fd4662d28a2da57b134626cc1314c18b
willshuffy <saputrawilly26@gmail.com>
2021-04-21 17:25:45 +0700

    Completing Detail Api setup and success run

--- app/src/main/java/com/umestudio/moovi/ui/DetailActivity.kt
+++ app/src/main/java/com/umestudio/moovi/ui/DetailActivity.kt
@@ -4,11 +4,14 @@ import android.os.Bundle
 import android.util.Log
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
 import com.umestudio.moovi.R
 import com.umestudio.moovi.model.Constant
 import com.umestudio.moovi.model.DetailResponse
 import com.umestudio.moovi.retrofit.ApiService
 import kotlinx.android.synthetic.main.activity_detail.*
+import kotlinx.android.synthetic.main.content_detail.*
+import kotlinx.android.synthetic.main.item_movie.view.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -19,17 +22,21 @@ class DetailActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail)
-        setSupportActionBar(toolbar)
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
+        setupView()
     }
 
     override fun onStart() {
         super.onStart()
         getMovieDetail()
+    }
+
+    private fun setupView(){
+
+        setContentView(R.layout.activity_detail)
+        setSupportActionBar(toolbar)
+
+        supportActionBar!!.title = ""
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
     }
 
     private fun getMovieDetail(){
@@ -53,6 +60,28 @@ class DetailActivity : AppCompatActivity() {
     }
 
     fun showMovie(detail: DetailResponse){
-        Log.d(TAG, "overviewResponse: ${detail.overview}")
+//        Log.d(TAG, "overviewResponse: ${detail.overview}")
+
+
+        val backdropPath = Constant.BACKDROP_PATH + detail.backdrop_path
+        Glide
+            .with(this)
+            .load(backdropPath)
+            .centerCrop()
+            .placeholder(R.drawable.placeholder_portrait)
+            .into(iv_backdrop)
+
+        tv_title.text = detail.title
+        tv_vote.text = detail.vote_average.toString()
+        tv_overview.text = detail.overview
+
+        for (genre in detail.genres!!){
+            tv_genre.text = "${genre.name}"
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        finish()
+        return super.onSupportNavigateUp()
     }
 }

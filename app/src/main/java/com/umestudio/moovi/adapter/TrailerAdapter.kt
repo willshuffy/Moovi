f05cdd13701d135f8902b55d5cfd7cb0e5acddef
willshuffy <saputrawilly26@gmail.com>
2021-04-22 17:22:18 +0700

    setup adapter trailer and showing videos trailer

---
+++ app/src/main/java/com/umestudio/moovi/adapter/TrailerAdapter.kt
@@ -0,0 +1,48 @@
+package com.umestudio.moovi.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.umestudio.moovi.R
+import com.umestudio.moovi.model.Constant
+import com.umestudio.moovi.model.TrailerModel
+import kotlinx.android.synthetic.main.item_trailer.view.*
+
+class TrailerAdapter(var videos: ArrayList<TrailerModel>, var listener: OnAdapterListener):
+    RecyclerView.Adapter<TrailerAdapter.viewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int)= viewHolder (
+        LayoutInflater.from(parent.context).inflate(R.layout.item_trailer, parent, false)
+    )
+
+
+    override fun getItemCount() = videos.size
+
+    override fun onBindViewHolder(holder: viewHolder, position: Int) {
+        holder.bind(videos[position])
+
+        holder.view.tv_trailer.setOnClickListener {
+            listener.onPlay(videos[position].key!!)
+        }
+    }
+
+    inner class viewHolder(val view: View): RecyclerView.ViewHolder(view) {
+        fun bind(trailer: TrailerModel){
+            view.tv_trailer.text = trailer.name
+        }
+
+    }
+
+    fun setData(loadVideos: List<TrailerModel>){
+        videos.clear()
+        videos.addAll(loadVideos)
+        notifyDataSetChanged()
+        listener.onLoad(loadVideos[0].key!!)
+    }
+
+    interface OnAdapterListener{
+        fun onLoad(key: String)
+        fun onPlay(key: String)
+    }
+}
--- app/src/main/java/com/umestudio/moovi/ui/TrailerActivity.kt
+++ app/src/main/java/com/umestudio/moovi/ui/TrailerActivity.kt
@@ -3,10 +3,14 @@ package com.umestudio.moovi.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.umestudio.moovi.R
+import com.umestudio.moovi.adapter.TrailerAdapter
 import com.umestudio.moovi.model.Constant
 import com.umestudio.moovi.model.TrailerResponse
 import com.umestudio.moovi.retrofit.ApiService
+import kotlinx.android.synthetic.main.activity_trailer.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -15,11 +19,14 @@ class TrailerActivity : AppCompatActivity() {
 
     private val TAG: String = "TrailerActivity"
 
+    lateinit var trailerAdapter: TrailerAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_trailer)
         setupView()
         setupListener()
+        setupRecyclerView()
     }
 
     override fun onStart() {
@@ -33,6 +40,26 @@ class TrailerActivity : AppCompatActivity() {
 
     private fun setupListener(){
 
+    }
+
+    private fun setupRecyclerView() {
+
+        trailerAdapter = TrailerAdapter(arrayListOf(), object : TrailerAdapter.OnAdapterListener{
+            override fun onLoad(key: String) {
+
+            }
+
+            override fun onPlay(key: String) {
+
+            }
+
+
+        })
+
+        rv_trailer.apply {
+            layoutManager = LinearLayoutManager(context)
+            adapter = trailerAdapter
+        }
     }
 
     private fun getMovieTrailer(){
@@ -59,17 +86,15 @@ class TrailerActivity : AppCompatActivity() {
     private fun showLoading(loading: Boolean){
         when(loading){
             true -> {
-
+                progressbar_trailer.visibility = View.VISIBLE
             }
             false -> {
-
+                progressbar_trailer.visibility = View.GONE
             }
         }
     }
 
     private fun showTrailer(trailer: TrailerResponse){
-        for (res in trailer.results){
-            Log.d(TAG, "nameVideo: ${res.name}")
-        }
+        trailerAdapter.setData(trailer.results)
     }
 }
